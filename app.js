a06b644930632300b08f7383e54bf76a5b2f067f
TekniqueJoseph <joejacksun@gmail.com>
2023-05-13 18:39:39 -0400

    expieramneted splice

--- app.js
+++ app.js
@@ -37,7 +37,13 @@ hobbies.shift() // removes from start
 console.log(hobbies);
 
 hobbies[1] = 'Coding'
-hobbies[5] ='reading'
-console.log(hobbies);
+// hobbies[5] ='reading'
+console.log(hobbies, hobbies[4]);
 
+hobbies.splice(1, 0, 'Good Food')
+console.log(hobbies)
 
+const removedElements = hobbies.splice(-2, 1)
+
+console.log(hobbies)
+console.log(removedElements)
