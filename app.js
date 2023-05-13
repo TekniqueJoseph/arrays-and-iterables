8834f55c0e540d6ed1762fa321c443ce32071017
TekniqueJoseph <joejacksun@gmail.com>
2023-05-13 12:27:05 -0400

    expieramneted with Array.from and for of loop

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const numbers = [1, 2, 3];
 console.log(numbers)
 
-const moreNumbers = new Array(5) //defines the array length
-console.log(moreNumbers)
+// const moreNumbers = new Array(5) //defines the arraylength. new keyword optional
+// console.log(moreNumbers)
+
+// const yetMoreNumbers = Array.of(1,2);
+// console.log(yetMoreNumbers)
+
+const listItems = document.querySelectorAll('#mainList li')
+console.log(listItems)
+
+const li = Array.from(listItems) // converts listItems from NodeList 2 Array
+console.log(li);
+
+for (let fruit of li) {
+    console.log(fruit) // displays the li's
+}
+
