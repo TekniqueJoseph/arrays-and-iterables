d9df8e74504e97db77e5de9a6f4b7936fa0f198d
TekniqueJoseph <joejacksun@gmail.com>
2023-05-14 15:04:22 -0400

    added completed assignment code

--- app.js
+++ app.js
@@ -77,18 +77,39 @@ for (let fruit of li) {
 
 // console.log(maxIndex)
 
-const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
-const taxAdjustedPrices = [];
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
 
-// for (const price of prices) {
-//     taxAdjustedPrices.push(price*(1 + tax)) 
-// }
+// // for (const price of prices) {
+// //     taxAdjustedPrices.push(price*(1 + tax)) 
+// // }
 
-prices.forEach((price, idx, prices) => {
-    const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)}
-    taxAdjustedPrices.push(priceObj) 
-});
+// prices.forEach((price, idx, prices) => {
+//     const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)}
+//     taxAdjustedPrices.push(priceObj) 
+// });
+
+// // console.log(taxAdjustedPrices)
+// console.log(taxAdjustedPrices)
+
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+
+// const taxAdjustedPrices = prices.map((price, idx, prices) => {
+//     const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)}
+//     return priceObj 
+// });
 
 // console.log(taxAdjustedPrices)
-console.log(taxAdjustedPrices)
+// console.log(prices, taxAdjustedPrices)
+
+const array = [1, 2, 3]
+function transformToObjects(numberArray) {
+    const newArray = numberArray.map((num) =>{
+        const newNum = {val: num}
+        return newNum
+    })
+    console.log(newArray)
+}
+transformToObjects(array)
