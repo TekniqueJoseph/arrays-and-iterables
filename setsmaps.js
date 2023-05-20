4df92fc2bac36e04dc102bf2f513727447175662
TekniqueJoseph <joejacksun@gmail.com>
2023-05-20 12:39:38 -0400

    added Map data

--- setsmaps.js
+++ setsmaps.js
@@ -12,3 +12,14 @@
 
 const person1 = {name: 'Jasmine'}
 const person2 = {name: 'Lisa'}
+
+const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]])
+
+personData.set(person2, [{date: 'two weeks ago', price: 100}])
+
+// console.log(personData)
+// console.log(personData.get(person1))
+
+for (const entry of personData.entries()){
+    console.log(entry)
+}
