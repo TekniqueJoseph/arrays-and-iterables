3dd2d5bfbac36642f8a94736e2dc4e18dd565d96
TekniqueJoseph <joejacksun@gmail.com>
2023-05-20 12:19:11 -0400

    added set data

--- setsmaps.js
+++ setsmaps.js
@@ -1,11 +1,14 @@
-const ids = new Set(['Hi', 'from', 'set!', ]);
-ids.add(2);
-if (ids.has('Hi')){
-    ids.delete('Hi')
-}
+// const ids = new Set(['Hi', 'from', 'set!', ]);
+// ids.add(2);
+// if (ids.has('Hi')){
+//     ids.delete('Hi')
+// }
 
-// console.log(ids.entries())
+// // console.log(ids.entries())
 
-for (const entry of ids.entries()){
-    console.log(entry[0])
-}
+// for (const entry of ids.entries()){
+//     console.log(entry[0])
+// }
+
+const person1 = {name: 'Jasmine'}
+const person2 = {name: 'Lisa'}
