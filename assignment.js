636a34e330b8b0babaa186ff300cafa032721236
TekniqueJoseph <joejacksun@gmail.com>
2023-05-20 21:06:22 -0400

    worked on assignment

--- assignment.js
+++ assignment.js
@@ -29,38 +29,43 @@ const findMax = ((...args)=>{
             return -1;
         } 
     }) 
-    console.log('2',biggest.pop(), biggest.shift())
+    console.log('2',biggest.pop())
 })
 findMax(2,8,55,99,22,36)
 findMax(...array)
 
-// const findMax = array.sort((a,b)=>{
-//         if (a > b) {
-//         return 1;
-//     } else if (a === b) {
-//         return 0;
-//     } else {
-//         return -1;
-//     }
-// })
+const [smallest, largest,...other] = array; 
+const findMax3 = ((...args)=>{
+    const biggest = args.sort((a,b)=>{
+        if (a > b) {
+        return 1;
+        } else if (a === b) {
+            return 0;
+        } else {
+            return -1;
+        } 
+    })
+    let smallest = biggest.shift()
+    let largest = biggest.pop()
+    // return [biggest.shift(),biggest.pop(),]
+    let finish = [smallest, largest]
+    const [small, large] = finish
+    const little = small;
+    const big = large;
+    console.log(little)
+    console.log(big)
+    return finish
 
-// // console.log(findMax.pop())
+})
+console.log(findMax3(2,8,55,99,22,36))
+console.log(findMax3(...array))
+// findMax3(smallest,largest)
+// console.log(findMax3)
 
-// const findMax2 = ((a,b)=>{
-//     if (a > b) {
-//         return 1;
-//     } else if (a === b) {
-//         return 0;
-//     } else {
-//         return -1;
-//     }
-// })
+const ids = new Set([1,2 ,3]);
+ids.add(2);
 
+console.log(ids)
 
-
-
-
-
-// console.log(findMax2(...array))
 
 
